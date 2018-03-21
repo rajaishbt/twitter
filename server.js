cb0c37eda8903a892029e6cc35dc2b2164f8ce31
rajaishbt <rajaishbt@gmail.com>
2018-03-22 03:25:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -119,6 +119,7 @@ app.get('*', function(req, res) {
   res.sendFile(pathToApp + '/index.html');
 });
 
-app.listen(3000, function () {
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
